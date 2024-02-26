ed485ec2ff2eda1a201a529da37f6acfc97194dd
Alana McKeel <amckeel@mtsgrp.net>
2024-02-26 15:19:48 -0500

    added controller

--- controllers/products.js
+++ controllers/products.js
@@ -3,14 +3,15 @@ const Product = require("../models/product");
 // testing controller
 const getAllProductsStatic = async (req, res) => {
   // throw new Error("testing async error");
-  const products = await Product.find({}); // whatever you put into the object is what will be searched, if left blank, will populate all products
+
+  const products = await Product.find({}).sort("-name"); // whatever you put into the object is what will be searched, if left blank, will populate all products
   res.status(200).json({ products, nbHits: products.length });
 };
 
 // live controller
 const getAllProducts = async (req, res) => {
   // console.log(req.query); testing to be sure the controller is working, using postmon to send a simple query, the results show in terminal
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
 
   const queryObject = {};
 
@@ -20,6 +21,10 @@ const getAllProducts = async (req, res) => {
 
   if (company) {
     queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
 
   console.log(queryObject);
